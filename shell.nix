e9f155095958c368636ed94bf24876dee2905ad3
Nick Novitski <nicknovitski@gmail.com>
2017-02-17 17:46:04 +1300

    Add write-good to development environment

--- shell.nix
+++ shell.nix
@@ -4,8 +4,18 @@ with pkgs;
 
 let
   pkg = callPackage ./. { inherit env; };
+  nodeEnv = import ./node-env.nix {
+    inherit (pkgs) stdenv python nodejs utillinux runCommand writeTextFile;
+  };
+  npkgs = import ./node-packages.nix {
+    inherit (pkgs) fetchurl fetchgit;
+    inherit nodeEnv;
+  };
 in
 
 stdenv.lib.overrideDerivation pkg (oldAttrs: {
+  buildInputs = oldAttrs.buildInputs ++ [
+    npkgs.write-good
+  ];
   shellHook = "alias s='jekyll serve -H 0.0.0.0'";
 })
